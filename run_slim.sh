a10ace782e3047c084bf96433b719b42e97bed2d
William Wesley Booker <wwbooker@longleaf-login4.its.unc.edu>
2022-10-22 00:30:33 -0400

    current settings work for expansion load in diploids and polyploids

--- run_slim.sh
+++ run_slim.sh
@@ -2,10 +2,10 @@
 #SBATCH -p general
 #SBATCH -N 1
 #SBATCH -n 1 
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=64G
 #SBATCH -J slim
 #SBATCH -o slim.%A.out
 #SBATCH -e slim.%A.err
 
-slim -seed 16948153676115 polyploid_all.slim
+slim pieschl_sim.slim
