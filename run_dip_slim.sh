9ff84fa5c244b728f9e52d1062530a727f5aadfa
William Wesley Booker <wwbooker@longleaf-login4.its.unc.edu>
2022-10-24 21:24:51 -0400

    fixed fitness_scalar bug

---
+++ run_dip_slim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH -n 1 
+#SBATCH --time=48:00:00
+#SBATCH --mem=16G
+#SBATCH -J slim
+#SBATCH -o slim.%A.out
+#SBATCH -e slim.%A.err
+
+slim diploid_sim.slim
