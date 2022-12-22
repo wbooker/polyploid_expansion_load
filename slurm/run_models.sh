7ec9419f2611849391fec8143c6de87c897c41c1
William Wesley Booker <wwbooker@longleaf-login4.its.unc.edu>
2022-12-22 13:29:17 -0500

    combined DFE and non-DFE into one script, fixed some output, and reorganized files

--- slurm/run_models.sh
+++ slurm/run_models.sh
@@ -1,6 +1,6 @@
 #run a series of models with different porameters
 INHERITANCE=("diploid" "allo" "auto")
-DOMINANCE=("additive" "overdominance_1" "overdominance_2" "overdominance_3")
+DOMINANCE=("DFE")
 K=(50)
 for i in "${INHERITANCE[@]}"
 do
@@ -8,41 +8,7 @@ do
     do
         for k in "${K[@]}"
         do
-            for j in {1..5}
-            do
-            sbatch slurm/run_slim.sh $i $d $k 0.000
-            done
-        done
-    done
-done
-
-INHERITANCE=("diploid" "auto" "allo")
-DOMINANCE=("recessive" "underdominance_1" "underdominance_2")
-K=(50)
-for i in "${INHERITANCE[@]}"
-do
-    for d in "${DOMINANCE[@]}"
-    do
-        for k in "${K[@]}"
-        do
-            for j in {1..5}
-            do
-            sbatch slurm/run_slim.sh $i $d $k 0.000
-            done
-        done
-    done
-done
-
-INHERITANCE=("auto")
-DOMINANCE=("duplex")
-K=(50)
-for i in "${INHERITANCE[@]}"
-do
-    for d in "${DOMINANCE[@]}"
-    do
-        for k in "${K[@]}"
-        do
-            for j in {1..5}
+            for j in {1..50}
             do
             sbatch slurm/run_slim.sh $i $d $k 0.000
             done
@@ -51,17 +17,42 @@ do
 done
 
 INHERITANCE=("diploid" "auto")
-DOMINANCE=("br_dd" "bd_dr")
+DOMINANCE=("recessive")
 K=(50)
+b_s=(0.000 0.0045)
 for i in "${INHERITANCE[@]}"
 do
     for d in "${DOMINANCE[@]}"
     do
         for k in "${K[@]}"
         do
-            for j in {1..5}
+            for b in "${b_s[@]}"
             do
-            sbatch slurm/run_slim.sh $i $d $k 0.005
+                for j in {1..50}
+                do
+                sbatch slurm/run_slim.sh $i $d $k $b
+                done
+            done
+        done
+    done
+done
+
+INHERITANCE=("diploid" "auto" "allo")
+DOMINANCE=("additive")
+K=(50)
+b_s=(0.000 0.0045)
+for i in "${INHERITANCE[@]}"
+do
+    for d in "${DOMINANCE[@]}"
+    do
+        for k in "${K[@]}"
+        do
+            for b in "${b_s[@]}"
+            do
+                for j in {1..50}
+                do
+                sbatch slurm/run_slim.sh $i $d $k $b
+                done
             done
         done
     done
