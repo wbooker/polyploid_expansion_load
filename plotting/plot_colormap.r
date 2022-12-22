0d750e971d77faa3e70c282e1edb3f6aa5738c2c
William Wesley Booker <wwbooker@longleaf-login4.its.unc.edu>
2022-12-21 23:10:17 -0500

    added site frequency and selection coeffecient frequency outputs, modified non DFE to allow prev outputs, got m1 and m2 proportions and presence working for all, even if no beneficial mutations are used in the model

--- plotting/plot_colormap.r
+++ plotting/plot_colormap.r
@@ -1,16 +1,19 @@
 avg_mat <- NULL
 track_mat <- NULL
-deme_plot <- 3000
+deme_plot <- 2000
 sim <- 5
-setwd("/proj/dschridelab/wwbooker/polyploid_expansion_load/output/test_12_15_2022/diploid_DFE_K-50_m-0.05_r-0.693147_u-2.5e-08_rho-2.5e-08_bs-0.0_ds--0.005_g-999999_start-2501/")
-dirs <- list.dirs(path = "/proj/dschridelab/wwbooker/polyploid_expansion_load/output/test_12_15_2022/diploid_DFE_K-50_m-0.05_r-0.693147_u-2.5e-08_rho-2.5e-08_bs-0.0_ds--0.005_g-999999_start-2501", full.names = TRUE)
-file <- "_meanFitness"
+setwd("/proj/dschridelab/wwbooker/polyploid_expansion_load/output/test_12_19_2022/auto_DFE_K-50_m-0.05_r-0.693147_u-2.5e-08_rho-2.5e-08_bs-0.0_ds--0.005_g-999999_start-2501/")
+dirs <- list.dirs(path = "/proj/dschridelab/wwbooker/polyploid_expansion_load/output/test_12_19_2022/auto_DFE_K-50_m-0.05_r-0.693147_u-2.5e-08_rho-2.5e-08_bs-0.0_ds--0.005_g-999999_start-2501", full.names = TRUE)
+file <- "_popSize"
+
+##Note here just to remember to get presence for averaging from popSize, not from if value is 0 or not, since some measurements like heterozygosity will be 0 and that will mess up the averaging as it is currently
+
 #for(dir in dirs[c(seq(2,5),8,seq(11,21),seq(24,32))]){
 #for(dir in dirs[c(6,7)]){
 dir <- dirs
   for(i in c(1,2,3,4,5)){
     dat <- read.csv(paste(c(dir,"/",i,file,".csv"),sep="", collapse=""), row.names = 1)
-    dat <- dat[1:(deme_plot/10),1:850]
+    dat <- dat[1:(deme_plot/10),1:1000]
     track_mat_temp <- (dat != 0)*1
     if(i == 1){
       avg_mat <- dat
@@ -26,15 +29,15 @@ dir <- dirs
 
   library(fields)
   x = as.integer(row.names(dat))
-  y = seq(1:850)
+  y = seq(1:1000)
   z = avgs_mat
-  png(file=paste(c(dir,"/","all",file,".png"),sep="", collapse=""), width = 600, height = 900)
+  png(file=paste(c(dir,"/","all",file,".png"),sep="", collapse=""), width = 700, height = 1100)
   #image.plot(x,y,as.matrix(z), zlim=c(0,200), xlab="Generation", ylab="Deme")
   #image.plot(x,y,as.matrix(z), zlim=c(0,50), xlab="Generation", ylab="Deme")
   #image.plot(x,y,as.matrix(z), zlim=c(1e-07, 0.0001), xlab="Generation", ylab="Deme")
-  #image.plot(x,y,as.matrix(z), zlim=c(0, 0.2), xlab="Generation", ylab="Deme")
+  image.plot(x,y,as.matrix(z), zlim=c(0, 0.6), xlab="Generation", ylab="Deme")
   #image.plot(x,y,as.matrix(z), zlim=c(0,75), xlab="Generation", ylab="Deme")
-  image.plot(x,y,as.matrix(z), zlim=c(0.8,1.05), xlab="Generation", ylab="Deme")
+  #image.plot(x,y,as.matrix(z), zlim=c(0.8,1.05), xlab="Generation", ylab="Deme")
   #image.plot(x,y,as.matrix(z), zlim=c(0, 0.05), xlab="Generation", ylab="Deme")
   #image.plot(x,y,as.matrix(z), zlim=c(0.000000001, 0.0001), xlab="Generation", ylab="Deme")
   #image.plot(x,y,as.matrix(z), zlim=c(0, 75), xlab="Generation", ylab="Deme")
--- slurm/run_test.sh
+++ slurm/run_test.sh
@@ -2,10 +2,15 @@
 #SBATCH -p general
 #SBATCH -N 1
 #SBATCH -n 1 
-#SBATCH --time=96:00:00
+#SBATCH --time=120:00:00
 #SBATCH --mem=16G
 #SBATCH -J slim
 #SBATCH -o slim.%A.out
 #SBATCH -e slim.%A.err
 
-slim -d K=51 -d "r=log(2)" -d mig_rate=0.05 -d u=2.5e-11 -d rho=2.5e-11 -d b_s=0.000 -d d_s=-0.005 -d "inheritance='dip'" -d "dom_pattern='recessive'" diploid_test.slim 
+INHERITANCE=${1}
+DOMINANCE=${2}
+K=$3
+BS=$4
+
+slim -d g_size=999999 -d K=$K -d "r=log(2)" -d mig_rate=0.05 -d u=2.5e-8 -d rho=2.5e-8 -d b_s=$BS -d d_s=-0.005 -d "inheritance='$INHERITANCE'" -d "dom_pattern='$DOMINANCE'" polyploid_all.slim 
