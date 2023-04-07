a3518c1a0c2fa3d7e43b08c8d247076b2072065c
William Wesley Booker <wwbooker@longleaf-login4.its.unc.edu>
2023-04-07 10:13:37 -0400

    testing new fitness stuff with older diploid scripts, trying to see why not working on new script

--- extract_core_fit.sh
+++ extract_core_fit.sh
@@ -2,5 +2,5 @@
 touch core_fits_2501.txt
 for i in {1..50}
 do
-    cat $i/${i}_log.csv | head -n 2 | tail -n 1 | awk -F, '{print $2}' >> core_fits_2501.txt
+    cat $i/${i}_log.csv | head -n 2 | tail -n 1 | awk -F, '{print $4}' >> edge_fits_2501.txt
 done
--- slurm/run_test.sh
+++ slurm/run_test.sh
@@ -8,9 +8,4 @@
 #SBATCH -o slim.%A.out
 #SBATCH -e slim.%A.err
 
-INHERITANCE=${1}
-DOMINANCE=${2}
-K=$3
-BS=$4
-
-slim -d g_size=999999 -d K=$K -d "r=log(2)" -d mig_rate=0.05 -d u=2.5e-8 -d rho=2.5e-8 -d b_s=$BS -d d_s=-0.005 -d "inheritance='$INHERITANCE'" -d "dom_pattern='$DOMINANCE'" polyploid_all.slim 
+slim test_dip_fitEffect.slim 
