b6cad42a6ffe9f37aa5015b7ded85d4e369e6f29
William Wesley Booker <wwbooker@longleaf-login4.its.unc.edu>
2023-04-07 18:23:47 -0400

    so far everything working, moving to DFE

--- slurm/run_slim.sh
+++ slurm/run_slim.sh
@@ -13,6 +13,5 @@
 INHERITANCE=${1}
 DOMINANCE=${2}
 K=$3
-BS=$4
 
-slim -d g_size=999999999 -d K=$K -d "r=log(2)" -d mig_rate=0.1 -d u_del=1e-11 -d u_ben=1e-12 -d rho=1e-11 -d b_s=$BS -d d_s=-0.001472 -d "inheritance='$INHERITANCE'" -d "dom_pattern='$DOMINANCE'" polyploid_all_DFE.slim 
+slim -d g_size=999999999 -d K=$K -d "r=log(2)" -d mig_rate=0.05 -d u_del=2.5e-11 -d u_ben=2.5e-12 -d b_s=0.005 -d d_s=-0.005 -d "inheritance='$INHERITANCE'" -d "dom_pattern='$DOMINANCE'" polyploid_all_DFE.slim 
